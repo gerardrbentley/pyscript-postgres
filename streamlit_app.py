2b82d85e8f5ddaed9dacc320775276e6b62f7491
Gerard Bentley <gerardrbentley@gmail.com>
2022-05-05 15:17:55 -0700

    url for deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.header("Streaming Postgres Inserts to Streamlit!")
 async def main():
     client = httpx.AsyncClient()
     async with client.stream(
-        "GET", "http://localhost:8080/listen_notes", timeout=None
+        "GET", "https://notes-api.gerardbentley.com/listen_notes", timeout=None
     ) as response:
         async for chunk in response.aiter_bytes():
             st.json(json.loads(chunk))
