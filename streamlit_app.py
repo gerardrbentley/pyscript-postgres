18dd6458326873191f54c53c4bf08dc9d3b73bd3
Gerard Bentley <gerardrbentley@gmail.com>
2022-05-07 15:10:11 -0700

    change: logging

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,23 @@ import json
 import httpx
 import asyncio
 import streamlit as st
+import logging
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
 st.header("Streaming Postgres Inserts to Streamlit!")
 
 
 async def main():
     client = httpx.AsyncClient()
+    log.info("Running main")
     async with client.stream(
         "GET", "https://notes-api.gerardbentley.com/listen_notes", timeout=None
     ) as response:
+        log.info("Listening")
         async for chunk in response.aiter_bytes():
+            log.info(f"processing: {str(chunk)}")
             st.json(json.loads(chunk))
 
 
